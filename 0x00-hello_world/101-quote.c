367f573d802d2d3a86bda153149dc8bf68587ef2
olly222 <adelabijacob@gmail.com>
2022-09-09 10:57:33 -0700

    my first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,18 +9,3 @@ int main(void)
     write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);   
     return (1);
 }
-
-
-#include <stdio.h>
-
-int main(void)
-{
- int a = 5;
- int b = 4;
-
- for (a < b;;)
- {
-   printf("%d\n", a++);
- }
- }
-
